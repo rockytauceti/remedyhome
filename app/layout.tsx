c50df4736e5824eeb1cf664ed4b3e084530ef83f
Rocky Tauceti <rockytauceti@gmail.com>
2026-06-23 01:30:22 -0400

    Add Google Tag Manager container

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Lora, DM_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 import "./globals.css";
 
 const lora = Lora({
@@ -48,6 +49,10 @@ export default function RootLayout({
     >
       <html lang="en" className={`${lora.variable} ${dmSans.variable} h-full antialiased`}>
         <body className="min-h-full flex flex-col bg-stone-50 text-stone-900">
+          <Script id="gtm-init" strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5GLB2RHV');`}</Script>
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GLB2RHV" height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
+          </noscript>
           {children}
         </body>
       </html>
